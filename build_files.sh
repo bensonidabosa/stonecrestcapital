51e65022d5d519aa7068f10d30f0dd996f74bae1
NephygTdkingz <tdkingzict@gmail.com>
2026-06-10 14:26:41 +0100

    added vercel new way

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
-python3.9 -m ensurepip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python -m ensurepip
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput
